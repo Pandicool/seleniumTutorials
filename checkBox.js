888c8c4c490dbc22d879fc0bb846264120bec5bd
Shashi95 <shashikanth.qa22@gmail.com>
2022-05-04 14:32:25 -0400

    changed Timeout

--- checkBox.js
+++ checkBox.js
@@ -5,7 +5,7 @@ async function demoQa(){
     let driver = await new Builder().forBrowser('chrome').build();
     await driver.get("https://demoqa.com/checkbox");
     await driver.findElement(By.xpath("(//*[@id='item-2'])[1]")).click();
-    await driver.sleep();
+    await driver.sleep(4000);
     await driver.findElement(By.xpath("//label[@for='impressiveRadio']")).click();
 
 }
