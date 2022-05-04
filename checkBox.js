c8e14127ea6f2612afa334f49c99dab784f3386e
Shashi95 <shashikanth.qa22@gmail.com>
2022-05-04 14:26:44 -0400

    added: after sleep

--- checkBox.js
+++ checkBox.js
@@ -5,7 +5,7 @@ async function demoQa(){
     let driver = await new Builder().forBrowser('chrome').build();
     await driver.get("https://demoqa.com/checkbox");
     await driver.findElement(By.xpath("(//*[@id='item-2'])[1]")).click();
-    await driver.sleep(2000);
+    await driver.sleep();
     await driver.findElement(By.xpath("//label[@for='impressiveRadio']")).click();
 
 }
